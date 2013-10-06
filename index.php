c2739e01f15efca91d55a39e027f0cac2da366d7
Sean Connelly <sean@seanconnelly.me>
2013-10-05 19:44:30 -0700

    Geometry.

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
     position: relative; 
     padding: 100px 0;
 }
-#home { 
-    background: url(wp-content/themes/dotme/assets/images/home.png) 50% 0 fixed; 
+#content { 
+    background: url(wp-content/themes/dotme/assets/images/content.png) 50% 0 fixed; 
     height: auto;  
     margin: 0 auto; 
     width: 100%; 
@@ -18,8 +18,8 @@
     padding: 200px 0;
     color:#fff;
 }
-#about { 
-    background: url(wp-content/themes/dotme/assets/images/about.png) 50% 0 fixed; 
+#footer { 
+    background: url(wp-content/themes/dotme/assets/images/footer.png) 50% 0 fixed; 
     height: auto;
     margin: 0 auto; 
     width: 100%; 
@@ -36,13 +36,13 @@
     </div>
 </section>
 <!-- Section 2 -->
-<section id="home" data-speed="4" data-type="background">
+<section id="content" data-speed="4" data-type="background">
     <div class="container">
         More content goes here!
     </div>
 </section>
 <!-- Section 3 -->
-<section id="about" data-speed="2" data-type="background">
+<section id="footer" data-speed="2" data-type="background">
     <div class="container">
         This is the final section!
     </div>
